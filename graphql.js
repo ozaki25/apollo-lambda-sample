0102e67bfd00eb8b73a7dc4c71d5819cf9646014
ozaki <fs65ugumori@yahoo.co.jp>
2018-12-02 15:20:12 +0900

    add script

---
+++ graphql.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+});
